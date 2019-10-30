d9ebdc7e5cb12d2bac037c153c2e41871f2adf0a
adrimerican <adri.roos@yahoo.com>
2019-10-30 11:15:44 +0000

    Enjoy the adventure

---
+++ FavouriteCode.js
@@ -0,0 +1,27 @@
+alert("Be scared of Anoops driving");
+let user = prompt("But should you really be scared?");
+alert("You answered " + user);
+alert("Yes, you should be scared.");
+alert("However you still get in the car with her");
+alert("You guys start leaving the car park and out of nowhere comes a car heading towards you");
+let user2 = prompt("Do stop or keep going full speed?");
+alert(user2 + ", are you sure?");
+alert("Anoop: 'Ah do not worry, we will be fine'");
+alert("Andy: 'You are joking right'");
+let user3 = prompt("Who do you agree with?");
+if (user3 == "Anoop" || "anoop") {
+  alert("You go left and die");
+} else {
+  alert("You: Andy, she's not joking but we have no choice.");
+}
+alert("The car starts to turn and move into the oncoming car")
+let user4 = prompt("Do you start to scream or grab the wheel and swerve?");
+if (user4 === "scream" || "Scream") {
+  alert("You: 'Andy stop crying'");
+} else {
+  alert("You die from stupidity");
+}
+alert("Andy: I cannot, this is why I always drive.");
+alert("You: Well, looks like we are stuck in the tiny death trap now. Tell my parents I love them.");
+alert("You all arrive safely to the Garden centre and buy a vase for Tom which he loves and thanks you for risking your life for him.");
+alert("Not before Anoop nearly drove down a one way street on to the motorway THE WRONG WAY.")
